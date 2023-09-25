c3edaf3149660c5b6590f755a0df6a0b1095fd4a
decagon <chiorlujack@gmail.com>
2023-09-25 04:18:12 +0100

    Deployment

--- src/main/java/com/cardmonix/cardmonix/configurations/SecurityConfig.java
+++ src/main/java/com/cardmonix/cardmonix/configurations/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,45 +22,35 @@ public class SecurityConfig {
     @SneakyThrows
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
-        httpSecurity
-                .cors()
-                .and()
-                .csrf()
-                .disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(authEntryPoint)
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        "/api/v1/auth/**","/api/v1/password/**","/api/v1/giftcards/**",
-                        "/v2/api-docs",
-                        "/v3/api-docs",
-                        "/v3/api-docs/**",
-                        "/swagger-resources",
-                        "/swagger-resources/**",
-                        "/configuration/ui",
-                        "/configuration/security",
-                        "/swagger-ui/**",
-                        "/webjars/**",
-                        "/swagger-ui.html"
-                )
-                .permitAll()
-                .requestMatchers("/api/v1/user/veiw-coin","/api/v1/user/search-coin")
-                .permitAll()
-                .requestMatchers("/api/v1/user/user-details","/upload-picture","/update-profile")
-                .hasAnyAuthority("USER","ADMIN")
+        httpSecurity.cors(cor -> cor.configure(httpSecurity));
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.authorizeHttpRequests(authorize ->
+                authorize.requestMatchers(
+                                "/api/v1/auth/**", "/api/v1/password/**", "/api/v1/giftcards/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
+                        )
+                        .permitAll()
+                        .requestMatchers("/api/v1/user/veiw-coin", "/api/v1/user/search-coin")
+                        .permitAll()
+                        .requestMatchers("/api/v1/user/user-details", "/upload-picture", "/update-profile")
+                        .hasAnyAuthority("USER", "ADMIN")
 
-                .requestMatchers("/api/v1/admin/**")
+                        .requestMatchers("/api/v1/admin/**")
 
-                .hasAuthority("ADMIN")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
+                        .hasAuthority("ADMIN")
+                        .anyRequest()
+                        .authenticated()
+                        .and()
+                        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class));
         return httpSecurity.build();
     }
 }
